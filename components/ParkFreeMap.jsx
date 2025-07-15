3ea2aff8fff8b7a55fd65b302fdeb08d908da0b1
ParkFree1 <sharaf554@gmail.com>
2025-07-15 22:07:59 +0100

    Update ParkFreeMap.jsx
    Remove broken @/components/ui imports

--- components/ParkFreeMap.jsx
+++ components/ParkFreeMap.jsx
@@ -12,13 +12,11 @@ const ParkFreeMap = () => {
       const map = L.map(mapRef.current).setView([51.505, -0.09], 13);
 
       L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker([51.505, -0.09])
-        .addTo(map)
-        .bindPopup("You are here")
-        .openPopup();
+      L.marker([51.505, -0.09]).addTo(map).bindPopup("You are here").openPopup();
     }
   }, []);
 
